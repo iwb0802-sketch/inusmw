06287430485f858a1db0f6b93339cc52d76818e4
iwb0802-sketch <iwb0802@gmail.com>
2026-07-12 05:22:35 +0000

    fix: AI 상담 위젯 첫 화면부터 바로 노출 (스크롤 대기 제거)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
-          <AiConsultWidget bottomOffset={88} showAfterScroll />
+          <AiConsultWidget bottomOffset={88} />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
